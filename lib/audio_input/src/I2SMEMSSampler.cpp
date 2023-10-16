79033dbfe4a6201fc1a5a8ef90a5bf7f3ee22c1f
OOHehir <40183643+OOHehir@users.noreply.github.com>
2023-10-16 12:04:49 +0100

    Changes

--- lib/audio_input/src/I2SMEMSSampler.cpp
+++ lib/audio_input/src/I2SMEMSSampler.cpp
@@ -30,14 +30,25 @@ void I2SMEMSSampler::configureI2S()
 
 bool I2SMEMSSampler::register_wavfilewriter(WAVFileWriter *ext_writer){
     writer = ext_writer;
-    if (writer == nullptr)
+
+    if (writer == nullptr){
+        ESP_LOGE(TAG, "register_wavfilewriter() - WAVFileWriter not registered");
         return false;
-    else
+    }else{
         return true;
+    }
+        
 }
 
 int I2SMEMSSampler::read(int count)
 {
+    //ESP_LOGI(TAG, "I2SMEMSSampler::read()");
+
+    if(writer == nullptr){
+        ESP_LOGE(TAG, "read() - WAVFileWriter not registered");
+        return 0;
+    }
+
     // read from i2s
     int32_t *raw_samples = (int32_t *)malloc(sizeof(int32_t) * count);
 
@@ -51,13 +62,19 @@ int I2SMEMSSampler::read(int count)
     
     i2s_read(m_i2sPort, raw_samples, sizeof(int32_t) * count, &bytes_read, portMAX_DELAY);
     
-    // samples_read is the number of 32 bit samples read = 4096
-    // bytes_read is the number of bytes read = 1024
+    // bytes_read = number of bytes read = 4096
+    // samples_read = number of 32 bit samples read = 1024
     
     int samples_read = bytes_read / sizeof(int32_t);
+
+    //ESP_LOGI(TAG, "samples_read = %d", samples_read);  
     
     for (int i = 0; i < samples_read; i++)
-    {
+    {   
+        
+        //ESP_LOGI(TAG, "buffer_idx[] = %d", writer->buffer_idx[writer->buffer_active]);
+        //ESP_LOGI(TAG, "buffer_active = %d", writer->buffer_active);
+        
         // For the SPH0645LM4H-B MEMS microphone
         // The Data Format is I2S, 24-bit, 2’s compliment, MSB first.
         // The data precision is 18 bits; unused bits are zeros.
@@ -65,10 +82,19 @@ int I2SMEMSSampler::read(int count)
         // We need to store data in 16 bits so need to drop lower 16 bits
 
         // Store into wav file buffer
-        writer->buffer[writer->buffer_active][writer->buffer_idx[writer->buffer_active]] = raw_samples[i] >> (32 - 16);
-
+        if (writer->buffer_idx[writer->buffer_active] < writer->buffer_size){
+            writer->buffer[writer->buffer_active][writer->buffer_idx[writer->buffer_active]] = raw_samples[i] >> 11;
+            writer->buffer_idx[writer->buffer_active]++;
+        }
+        else{
+            writer->buffer_is_full();  // Swap buffers and set buffer_ready_to_save = true
+            writer->write();
+        }
+        
+        
+        
         // Store into edge-impulse buffer
-
+        // sampleBuffer[i] = raw_samples[i] >> 11;
 
     }
 
