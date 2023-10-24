e98f57b738ca1045a5b77eea8803eb0c0c0c426c
OOHehir <40183643+OOHehir@users.noreply.github.com>
2023-10-24 17:21:10 +0100

    New model (4Khz)

--- src/edge-impulse-sdk/classifier/ei_classifier_config.h
+++ src/edge-impulse-sdk/classifier/ei_classifier_config.h
@@ -19,6 +19,14 @@
 #define _EI_CLASSIFIER_CONFIG_H_
 
 // clang-format off
+
+// This is a file that's only used in benchmarking to override HW optimized kernels
+#ifdef __has_include
+    #if __has_include("source/benchmark.h")
+    #include "source/benchmark.h"
+    #endif
+#endif
+
 #if EI_CLASSIFIER_TFLITE_ENABLE_SILABS_MVP == 1
     #define EI_CLASSIFIER_TFLITE_ENABLE_CMSIS_NN        0
     #define EI_CLASSIFIER_TFLITE_LOAD_CMSIS_NN_SOURCES  1
@@ -67,8 +75,15 @@
 
 #ifndef EI_CLASSIFIER_TFLITE_ENABLE_ESP_NN
     #if defined(ESP32)
+        #include "sdkconfig.h"
         #define EI_CLASSIFIER_TFLITE_ENABLE_ESP_NN      1
+        #define ESP_NN                                  1
     #endif // ESP32 check
+    #if defined(CONFIG_IDF_TARGET_ESP32S3)
+        #define EI_CLASSIFIER_TFLITE_ENABLE_ESP_NN_S3      1
+    #endif // ESP32S3 check
+#else
+    #define ESP_NN                                  1
 #endif
 
 // no include checks in the compiler? then just include metadata and then ops_define (optional if on EON model)
